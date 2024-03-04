1e75e767498458f9d908f1c10985414dedbaf486
sandeep <sandeep-chauhan-self>
2024-03-04 16:25:09 +0530

    Q2 - challenges/java-arraylist/problem

---
+++ HackerRank/Q2.java
@@ -0,0 +1,44 @@
+package DSA.HackerRank;
+
+import java.util.*;
+
+public class Q2 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        ArrayList<int[]> list = new ArrayList<>();
+        ArrayList<int[]> list2 = new ArrayList<>();
+        int inputCount = Integer.parseInt(s.nextLine());
+
+        while (inputCount > 0) {
+            inputCount--;
+            String input = s.nextLine();
+            String[] inputString = input.split(" ");
+            int[] array = Arrays.stream(inputString).mapToInt(Integer::parseInt).toArray();
+            list.add(array);
+        }
+
+        int outputCount = Integer.parseInt(s.nextLine());
+
+        while (outputCount > 0) {
+            outputCount--;
+            String input = s.nextLine();
+            String[] inputString = input.split(" ");
+            int[] array = Arrays.stream(inputString).mapToInt(Integer::parseInt).toArray();
+            list2.add(array);
+        }
+
+        for (int[] is : list2) {
+            try {
+                int[] value = list.get(is[0] - 1);
+                System.out.println(value[is[1]]);
+
+            } catch (Exception e) {
+                System.out.println("ERROR!");
+            }
+
+        }
+
+        s.close();
+    }
+}
