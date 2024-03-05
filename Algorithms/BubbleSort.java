0cefe3cdd2d211a60cbf656c5dbbf5819fcb6015
sandeep <sandeep-chauhan-self>
2024-03-05 19:19:30 +0530

    BubbleSort

--- Algorithms/BubbleSort.java
+++ Algorithms/BubbleSort.java
@@ -3,35 +3,33 @@ package DSA.Algorithms;
 import java.util.Arrays;
 
 public class BubbleSort {
-
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 57, 12, 4, 57, 1, 3, 8, 9, 0,99,99 };
-        //int[] arr = { 1, 2, 3,4,5,6,7,8,9 };
-
-        sort(arr);
+        int[] arr = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+        // int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int target = 4;
+        sort(arr, target);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void sort(int[] arr) {
-        int count = 0;
-        boolean swapped = false;
+    private static void sort(int[] arr, int target) {
         for (int i = 0; i < arr.length; i++) {
-
+            boolean swapped = false;
             for (int j = 1; j < arr.length - i; j++) {
-                count++;
                 if (arr[j] < arr[j - 1]) {
-                    // swap logic
-                    
                     swapped = true;
-                    int temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
+                    swap(arr, j, j - 1);
                 }
             }
-            if(!swapped){
+
+            if (!swapped)
                 break;
-            }
         }
-        System.out.println(count);
     }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
 }
