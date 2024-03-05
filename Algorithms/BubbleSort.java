23e25036fc234ed2a5fb03251f5a8bc011ff9ed5
sandeep <sandeep-chauhan-self>
2024-03-05 19:22:01 +0530

    Bubble Sort

--- Algorithms/BubbleSort.java
+++ Algorithms/BubbleSort.java
@@ -6,12 +6,11 @@ public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
         // int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        int target = 4;
-        sort(arr, target);
+        sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    private static void sort(int[] arr, int target) {
+    private static void sort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             boolean swapped = false;
             for (int j = 1; j < arr.length - i; j++) {
